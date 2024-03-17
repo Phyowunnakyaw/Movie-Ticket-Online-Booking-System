fb1763b4863f1c31c25f33008da84e12752712d7
phyo wunna kyaw <phyowunnakyaw276@gmail.com>
2024-03-17 10:40:53 -0800

    add inititalFiles

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import NavBar from "./components/NavBar"
+import Home from "./pages/Home"
+
+function App( ) {
+
+
+  return (
+        
+          <BrowserRouter >
+          
+               <NavBar />
+
+               <Routes>
+
+                       <Route path='/' element= { <Home /> }  />
+
+               </Routes>
+
+          </BrowserRouter>
+  )
+}
+
+export default App
